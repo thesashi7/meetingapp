680f7bf31e66ef4fdbc5a4c99af6004ff01c1300
sashi <sashi_thapaliya@hotmail.com>
2018-03-01 18:46:25 -0800

    Added EmployeeSchedule

---
+++ models/EmployeeSchedule.py
@@ -0,0 +1,32 @@
+from Model import Model
+from services.EmployeeScheduleService import EmployeeScheduleService
+from services.DatabaseService import DatabaseService
+
+class EmployeeSchedule(Model):
+
+   __tablename__ = 'employeeschedule'
+   __table_args__ = {'autoload':True, 'autoload_with':DatabaseService.DBEngine()}
+   service = EmployeeScheduleService()
+
+
+   @staticmethod
+   def getByEmployeeScheduleId(id):
+       employee_schedule= EmployeeSchedule.service.getByEmployeeScheduleId(id)
+       return employee_schedule
+
+   @staticmethod
+   def getByEmployeeId(emp_id):
+       employee_schedule= EmployeeSchedule.service.getByEmployeeId(emp_id)
+       return employee_schedule
+
+   @staticmethod
+   def add(employee_schedule):
+      EmployeeSchedule.service.add(employee_schedule)
+      return True
+
+   @staticmethod
+   def delete(employee_schedule):
+       return EmployeeSchedule.service.delete(employee_schedule)
+
+   def update(self):
+       EmployeeSchedule.service.update(self)
---
+++ services/EmployeeScheduleService.py
@@ -0,0 +1,37 @@
+from DatabaseService import DatabaseService
+
+class EmployeeScheduleService(DatabaseService):
+
+   def get(self, id, serialize = False):
+     from models.EmployeeSchedule import EmployeeSchedule
+     employee_schedule = None
+     employee_schedule = self.session.query(EmployeeSchedule).get(id)
+     if serialize:
+        return employee_schedule.serialize()
+     else:
+        return employee_schedule
+
+   def getByEmployeeId(self, emp_id):
+     from models.EmployeeSchedule import EmployeeSchedule
+     employee_schedule= self.session.query(EmployeeSchedule).filter(EmployeeSchedule.employee_id == str(emp_id)).all()
+     if(len(employee_schedule)>0):
+        return employee_schedule
+     return None
+
+   def add(self, employee_schedule):
+     from models.EmployeeSchedule import EmployeeSchedule
+     if isinstance(employee_schedule, EmployeeSchedule):
+        self.session.add(employee_schedule)
+        self.session.commit()
+
+   def delete(self, employee_schedule):
+     from models.EmployeeSchedule import EmployeeSchedule
+     if isinstance(employee_schedule, EmployeeSchedule):
+        self.session.delete(employee_schedule)
+        self.session.commit()
+
+
+   def update(self, employee_schedule):
+      from models.EmployeeSchedule import EmployeeSchedule
+      if isinstance(employee_schedule, EmployeeSchedule):
+          self.session.commit()
